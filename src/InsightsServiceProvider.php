3e6f215b198b4d6a632b52665348c13e779f486b
Ryan Ralphs <ryan.ralphs@enta.net>
2019-08-29 10:27:28 +0100

    version1-revised

--- src/InsightsServiceProvider.php
+++ src/InsightsServiceProvider.php
@@ -9,7 +9,7 @@ class InsightsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/files/EntanetInsights.php' => base_path('console/commands/EntanetInsights.php')
+            __DIR__ . '/files/EntanetInsights.php' => base_path('app/Console/Commands/EntanetInsights.php')
         ]);
     }
 }
